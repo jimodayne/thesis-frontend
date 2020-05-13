5707b37f96777060ea8e997676d154e43d318a3e
Thinh <tio212@gmail.com>
2020-05-13 16:32:58 +0700

    refactor: update style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from "react";
-import Nav from "./Nav";
-import "./css/scss/main.scss";
+import React from 'react';
+import Nav from './Nav';
+import './css/scss/main.scss';
 import mic_icon from './assets/mic_icon.svg';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
-  return (
-    <div className="main-thesis">
-      <Nav />
-      <Router>
-        <Switch>
-          <Route exact path="/"></Route>
-          {/* <Route exact path="/aboutus"></Route>
+    return (
+        <div className="main-thesis">
+            <Nav />
+            <Router>
+                <Switch>
+                    <Route exact path="/"></Route>
+                    {/* <Route exact path="/aboutus"></Route>
           <Route exact path="/deepspeech"></Route> */}
-        </Switch>
-      </Router>
-    </div>
-  );
+                </Switch>
+            </Router>
+        </div>
+    );
 };
 
 export default App;
