7f10d391daecd462223ab31a013ba2658ce4503d
Thinh <tio212@gmail.com>
2020-05-13 16:29:02 +0700

    intial: bulma

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
+import Nav from "./Nav";
 import "./css/scss/main.scss";
 
 const App = () => {
-  return <div className="main-thesis">Will add something inside here!</div>;
+  return (
+    <div className="main-thesis">
+      <Nav />
+      <div className="container">
+        <h1 className="title">Hello World!</h1>
+      </div>
+    </div>
+  );
 };
 
 export default App;
