147e0d731c565a554c7b61f1bee6c1701362a64f
Thinh Tran <thinhtd2@vng.com.vn>
2020-07-02 22:37:01 +0700

    feat: update UI, fetch

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -3,6 +3,7 @@ import MicRecorder from 'mic-recorder-to-mp3';
 import Dropzone from 'react-dropzone';
 import { Chip } from '@material-ui/core';
 import AddBoxIcon from '@material-ui/icons/AddBox';
+import { Ring } from 'react-spinners-css';
 
 const API = 'http://104.199.147.226:8000/';
 const recorder = new MicRecorder({
@@ -13,7 +14,8 @@ const Homepage = () => {
     const [isRecording, setIsRecording] = useState(false);
     const [preview, setPreview] = useState();
     const [file, setFile] = useState([]);
-    const [result, setResult] = useState('huhuhuhuhuh');
+    const [result, setResult] = useState(undefined);
+    const [isLoading, setLoading] = useState(false);
 
     const handleOnClick = () => {
         return isRecording ? stopRecording() : startRecording();
@@ -53,90 +55,110 @@ const Homepage = () => {
     };
 
     const handleSubmit = async () => {
-        const res = await fetch(API, {
+        const formData = new FormData();
+        console.log('file', file);
+        formData.append('file', file, file.name);
+        setLoading(true);
+        fetch(API, {
             method: 'POST',
-            headers: {
-                'Content-Type': 'text/html'
-            },
-            body: file
+            body: formData
+        }).then(res => {
+            setLoading(false);
+            if (res) {
+                res.json().then(data => {
+                    data && setResult(data);
+                });
+            }
         });
-        console.log(res);
-        return res.json();
     };
 
     return (
-        <div>
-            <section className="section" id="homepage">
-                <div className="container">
-                    <div className="record-button-container">
-                        <button
-                            // disabled={file.length > 0}
-                            className={'record-button ' + (isRecording ? 'is-recording' : '')}
-                            onClick={handleOnClick}
-                        >
-                            <img className="record-icon" src="./mic_icon.svg" alt="mic-wrapper" />
-                        </button>
-                    </div>
-                    {preview && (
-                        <audio
-                            controls
-                            className="audio"
-                            src={preview}
-                            style={{ marginBottom: '15px', display: 'block' }}
-                        >
-                            Your browser does not support the
-                            <code>audio</code> element.
-                        </audio>
-                    )}
-                </div>
-                <div className="vertical_break"></div>
-                <div className="container upload">
-                    <div className="dropzone">
-                        <Dropzone
-                            accept={['.wav', '.mp3']}
-                            multiple={false}
-                            // disabled={file.length > 0}
-                            onDrop={acceptedFile => setFile(acceptedFile)}
-                        >
-                            {({ getRootProps, getInputProps }) => (
-                                <div {...getRootProps()}>
-                                    <input {...getInputProps()} />
-                                    <div
-                                        variant="text"
-                                        className="record-button-container"
-                                        style={{ textTransform: 'none' }}
-                                    >
-                                        <div className="record-button">
-                                            <AddBoxIcon className="add_icon" />
-                                        </div>
-                                    </div>
-                                </div>
-                            )}
-                        </Dropzone>
-                    </div>
-                    <div className="file_container">
-                        {file.length > 0 && (
-                            <Chip label={file[0].name} onDelete={() => setFile([])}></Chip>
+        <section className="section" id="homepage">
+            <div className="container">
+                <div className="columns">
+                    <div className="column">
+                        <div className="record-button-container">
+                            <button
+                                // disabled={file.length > 0}
+                                className={'record-button ' + (isRecording ? 'is-recording' : '')}
+                                onClick={handleOnClick}
+                            >
+                                <img
+                                    className="record-icon"
+                                    src="./mic_icon.svg"
+                                    alt="mic-wrapper"
+                                />
+                            </button>
+                        </div>
+                        {preview && (
+                            <audio
+                                controls
+                                className="audio"
+                                src={preview}
+                                style={{ marginBottom: '15px', display: 'block' }}
+                            >
+                                Your browser does not support the
+                                <code>audio</code> element.
+                            </audio>
                         )}
                     </div>
+                    <div className="column">
+                        <div className="upload">
+                            <div className="dropzone">
+                                <Dropzone
+                                    accept={['.wav', '.mp3']}
+                                    multiple={false}
+                                    // disabled={file.length > 0}
+                                    onDrop={acceptedFile => {
+                                        setFile(acceptedFile[0]);
+                                    }}
+                                >
+                                    {({ getRootProps, getInputProps }) => (
+                                        <div {...getRootProps()}>
+                                            <input {...getInputProps()} />
+                                            <div
+                                                variant="text"
+                                                className="record-button-container"
+                                                style={{ textTransform: 'none' }}
+                                            >
+                                                <div className="record-button">
+                                                    <AddBoxIcon className="add_icon" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )}
+                                </Dropzone>
+                            </div>
+                            <div className="file_container">
+                                {file.length > 0 && (
+                                    <Chip label={file[0].name} onDelete={() => setFile([])}></Chip>
+                                )}
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </section>
-            <div className="btn_container">
-                <div className={'submit_btn_container'}>
-                    <button
-                        disabled={file.length === 0}
-                        className="submit_btn"
-                        onClick={handleSubmit}
-                    >
-                        Submit
-                    </button>
+
+                <div className="btn_container">
+                    <div className={'submit_btn_container'}>
+                        <button
+                            disabled={file.length === 0}
+                            className="submit_btn"
+                            onClick={handleSubmit}
+                        >
+                            Submit
+                        </button>
+                    </div>
                 </div>
+                <div className="loading-wrapper">{isLoading && <Ring color="#be97e8" />}</div>
+                {result && (
+                    <div className="result has-text-centered has-text-weight-semibold">
+                        <div style={{ fontSize: '24px', marginBottom: '8px' }}>Result:</div>
+
+                        <div className="has-text-weight-normal">{result}</div>
+                    </div>
+                )}
             </div>
-            <div className="result has-text-centered has-text-weight-semibold">
-                <div style={{ fontSize: '24px', marginBottom: '8px' }}>Result:</div>
-                <div className="has-text-weight-normal">{result}</div>
-            </div>
-        </div>
+        </section>
     );
 };
 
