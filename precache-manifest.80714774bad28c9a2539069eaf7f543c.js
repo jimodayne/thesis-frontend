ca236cf839fddc7b8f97a2d745dc730416e48246
Thinh Tran <thinhtd2@vng.com.vn>
2020-07-23 19:28:10 +0700

    Updates

---
+++ precache-manifest.80714774bad28c9a2539069eaf7f543c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f477981696f30b377235ebd5eba4eea9",
+    "url": "/thesis-frontend/index.html"
+  },
+  {
+    "revision": "de0db7f508772d849302",
+    "url": "/thesis-frontend/static/css/2.079ed09a.chunk.css"
+  },
+  {
+    "revision": "e00e73b8eca24fb316c6",
+    "url": "/thesis-frontend/static/css/main.17772b90.chunk.css"
+  },
+  {
+    "revision": "de0db7f508772d849302",
+    "url": "/thesis-frontend/static/js/2.32fd976e.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/thesis-frontend/static/js/2.32fd976e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e00e73b8eca24fb316c6",
+    "url": "/thesis-frontend/static/js/main.0b026a8c.chunk.js"
+  },
+  {
+    "revision": "4545238db9043553c3e9",
+    "url": "/thesis-frontend/static/js/runtime-main.427891c1.js"
+  }
+]);
