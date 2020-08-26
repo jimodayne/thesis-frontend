4e882a93905d2fa57d9f85e9d3679325cdf39f2e
Thinh Tran <thinhtd2@vng.com.vn>
2020-08-26 23:38:26 +0700

    Updates

--- precache-manifest.17dd9cacad7911d1fbe3123f64b531a0.js
+++ precache-manifest.17dd9cacad7911d1fbe3123f64b531a0.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b2a257e427b4fb8bc944f5ab2bcf9caf",
+    "revision": "f19b202f5900d09851a0987b3b924ade",
     "url": "/thesis-frontend/index.html"
   },
   {
-    "revision": "20d46e09c7c9adac6cad",
+    "revision": "aea1542df4af95b82fde",
     "url": "/thesis-frontend/static/css/2.079ed09a.chunk.css"
   },
   {
-    "revision": "ad6a506025e394ed38a5",
+    "revision": "bc7a4a6775bbac876196",
     "url": "/thesis-frontend/static/css/main.6234f395.chunk.css"
   },
   {
-    "revision": "20d46e09c7c9adac6cad",
-    "url": "/thesis-frontend/static/js/2.5ba40000.chunk.js"
+    "revision": "aea1542df4af95b82fde",
+    "url": "/thesis-frontend/static/js/2.4b41616e.chunk.js"
   },
   {
     "revision": "5e9b6f5c16b720e8a86875f97734673e",
-    "url": "/thesis-frontend/static/js/2.5ba40000.chunk.js.LICENSE.txt"
+    "url": "/thesis-frontend/static/js/2.4b41616e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ad6a506025e394ed38a5",
-    "url": "/thesis-frontend/static/js/main.7bd25dbb.chunk.js"
+    "revision": "bc7a4a6775bbac876196",
+    "url": "/thesis-frontend/static/js/main.9450c3e1.chunk.js"
   },
   {
     "revision": "4545238db9043553c3e9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/thesis-frontend/precache-manifest.5b2e9c119c39cf10b6d30b5e51139387.js"
+  "/thesis-frontend/precache-manifest.17dd9cacad7911d1fbe3123f64b531a0.js"
 );
 
 self.addEventListener('message', (event) => {
